8708fd8e35c7b57cb143b12403efa55c29f71d58
adi160 <araj21686@gmail.com>
2020-04-19 17:22:24 +0530

    Add calendar and book appointment by just clicking

--- app/javascript/packs/schedule_calendar.js
+++ app/javascript/packs/schedule_calendar.js
@@ -12,47 +12,77 @@
 //   } 
 // });
 
-// document.addEventListener('DOMContentLoaded', function() {
+$(document).on("turbolinks:load", function(){
 
-// var calendarEl = document.getElementById('calendar');
+  var calendarEl = document.getElementById('calendar');
 
-// var calendar = new FullCalendar.Calendar(calendarEl, {
-//   plugins: ['interaction', 'timeGrid', 'dayGrid', 'list' ],
-//   defaultView: 'timeGrid',
-//   visibleRange: function(currentDate) {
-//     // Generate a new date for manipulating in the next step
-//     var startDate = new Date(currentDate.valueOf());
-//     var endDate = new Date(currentDate.valueOf());
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: ['interaction', 'timeGrid', 'dayGrid', 'list' ],
+    defaultView: 'timeGrid',
+    visibleRange: function(currentDate) {
+      // Generate a new date for manipulating in the next step
+      var startDate = new Date(currentDate.valueOf());
+      var endDate = new Date(currentDate.valueOf());
 
-//     // Adjust the start & end dates, respectively
-//     startDate.setDate(startDate.getDate()); 
-//     endDate.setDate(endDate.getDate() + 4); 
+      // Adjust the start & end dates, respectively
+      startDate.setDate(startDate.getDate()); 
+      endDate.setDate(endDate.getDate() + 4); 
 
-//     return { start: startDate, end: endDate };
-//   },
-//   selectable: true,
-//   minTime: '10:00:00',
-//   maxTime: '14:00:00',
-//   defaultTimedEventDuration: '00:15:00',
-//   slotDuration: '00:15:00',
-//   slotLabelInterval: 15,
-//   selectable: true,
-//   header: {
-//     left: 'prev,next,today',
-//     center: 'title',
-//     right: 'timeGridDay,timeGridWeek,listDay'
-//   },
-//   dateClick: function(info) {
-//     alert('clicked ' + info.dateStr);
-//   },
-//   select: function(info) {
-//     alert('selected ' + info.startStr + ' to ' + info.endStr);
-//     console.log(info);
-//   },
-// });
+      return { start: startDate, end: endDate };
+    },
+    selectable: true,
+    minTime: '10:00:00',
+    maxTime: '18:00:00',
+    defaultTimedEventDuration: '00:15:00',
+    slotDuration: '00:15:00',
+    slotLabelInterval: 15,
+    header: {
+      left: 'prev,next,today',
+      center: 'title',
+      right: 'timeGridDay,timeGridWeek,listDay'
+    },
+    // dateClick: function(info) {
+    //   alert('clicked ' + info.dateStr);
+    // },
+    select: function(info) {
+      $('#newEvent').modal('show');
+      $('#submit').unbind();
+      $('#submit').on('click', function() {
+        var title = "Booked"
+        if (title) {
+            calendar.addEvent({
+            title: title,
+            start: info.start,
+          });
+          $('#newEvent').modal('hide');
+          }
+        else {
+          $('#newEvent').modal('hide');
+        }
+      });
+    },
+  });
+  calendar.render();
+});
 
-// calendar.render();
-// });
+// var newEvent = function(start) {
+//   $('#newEvent').modal('show');
+//   $('#submit').unbind();
+//   $('#submit').on('click', function() {
+//   var title = "Booked"
+//   if (title) {
+//     calendar.addEvent({
+//       title: 'Booked',
+//       start: date,
+//       allDay: true
+//     });
+//     $('#newEvent').modal('hide');
+//     }
+//   else {
+//     $('#newEvent').modal('hide');
+//   }
+//   });
+// }
 
 function changeFormatOfTime(time){
   var dt = new Date(time);
